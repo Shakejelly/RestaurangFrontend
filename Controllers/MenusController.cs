f888bc09fd9a0956b163cc7433491806d1d2e771
Fredrich Benedetti <fredrich93@gmail.com>
2024-09-17 00:17:55 +0200

    Add project files.

---
+++ Controllers/MenusController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RestaurangFrontend.Controllers
+{
+    public class MenusController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
