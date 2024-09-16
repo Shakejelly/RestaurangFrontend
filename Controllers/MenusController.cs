370560b28e73fdb8d7fd3faad8719c4e4e235840
Fredrich Benedetti <fredrich93@gmail.com>
2024-09-17 00:34:07 +0200

    .

--- Controllers/MenusController.cs
+++ Controllers/MenusController.cs
@@ -1,11 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RestaurangFrontend.Models;
 
 namespace RestaurangFrontend.Controllers
 {
     public class MenusController : Controller
     {
-        public IActionResult Index()
+        private readonly HttpClient _httpClient;
+        private string baseUrl = "https://localhost:7053/";
+
+        public MenusController(HttpClient httpClient)
         {
+            _httpClient = httpClient;
+        }
+        public async Task<IActionResult> Index()
+        {
+            ViewData["Title"] = "The Menu";
+
+            var respons = await _httpClient.GetAsync($"{baseUrl}api/Menu/getAllDishes");
+
+            var json = await respons.Content.ReadAsStringAsync();
+
+            var menuList = JsonConvert.DeserializeObject<List<Menu>>(json);
+
             return View();
         }
     }
---
+++ Models/Menu.cs
@@ -0,0 +1,10 @@
+﻿namespace RestaurangFrontend.Models
+{
+    public class Menu
+    {
+        public int Id { get; set; }
+        public string DishName { get; set; }
+        public string Description { get; set; }
+        public int DishPrice { get; set; }
+    }
+}
