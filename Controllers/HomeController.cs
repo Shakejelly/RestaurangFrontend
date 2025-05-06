8970fb705673874e0b6583a960c1bf9f5a0e6a64
Fredrich Benedetti <fredrich.benedettiluna@afry.com>
2025-05-06 23:42:06 +0200

    .

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -21,7 +21,10 @@ namespace RestaurangFrontend.Controllers
         {
             return View();
         }
-
+        public IActionResult ReactBooking()
+        {
+            return View();
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
